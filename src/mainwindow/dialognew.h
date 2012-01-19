9b3179bedfe11769302895d29d63961df7380344
Josh <Josh@Athena>
2012-01-18 16:36:16 -0800

    Adding in the resource file with the toolbar icons

---
+++ src/mainwindow/dialognew.h
@@ -0,0 +1,23 @@
+#ifndef DIALOGNEW_H
+#define DIALOGNEW_H
+#include <QDialog>
+#include <QWidget>
+#include "ui_dialognew.h"
+
+class dialognew : public QDialog, Ui::dialognew
+{
+    Q_OBJECT
+    int diagramType;
+public:
+    dialognew();
+    int getDiagramType();
+private slots:
+     void on_OkButton_accepted();
+     void on_UseCaseRadioButton_clicked();
+     void on_ClassRadioButton_clicked();
+     void on_ActivityRadioButton_clicked();
+     void on_InteractionRadioButton_clicked();
+     void on_StateRadioButton_clicked();
+};
+
+#endif // DIALOGNEW_H
