43f3c2f30ca339bf1989075411073ebe071bb1fe
Josh Armstrong <theartistformerlyknownasjosh@gmail.com?>
2012-02-01 22:19:37 -0800

    Backed out changeset 9a825336cdfd. Looks like you uploaded your entire user directory David

--- 150/lisp/lists.lisp
+++
@@ -1,4 +0,0 @@
-(defun splice (L1 L2) listp '((CAR L1) (CAR L2)))
-(if (eql (cdr L2) nil)
-(splice (cdr L1) (cdr L2))))
-
--- blah/puml/code/class_diagram_translation/constants.h
+++
@@ -1,6 +0,0 @@
-/*!
- * @file constants.h
- *      @brief allows for global behavioral changes
- */
-
-#define PRINT_DEBUG_MESSAGES
