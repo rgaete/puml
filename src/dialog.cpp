e9e6f283608f70ac994f791853a17a1aeb79d25b
Logan Evans <loganpevans@gmail.com>
2012-02-21 17:26:37 -0800

    Changed the .hgignore.

---
+++ src/dialog.cpp
@@ -0,0 +1,81 @@
+#include <QtGui>
+
+#include "dialog.h"
+#include "dialogpages.h"
+
+ConfigDialog::ConfigDialog()
+{
+    contentsWidget = new QListWidget;
+    contentsWidget->setViewMode(QListView::IconMode);
+    contentsWidget->setIconSize(QSize(96, 84));
+    contentsWidget->setMovement(QListView::Static);
+    contentsWidget->setMaximumWidth(128);
+    contentsWidget->setSpacing(12);
+
+    pagesWidget = new QStackedWidget;
+    pagesWidget->addWidget(new CreateNewPage);
+    pagesWidget->addWidget(new CreateOpenPage);
+
+    QPushButton *closeButton = new QPushButton(tr("Cancel"));
+
+    createIcons();
+    contentsWidget->setCurrentRow(0);
+
+    connect(closeButton, SIGNAL(clicked()), this, SLOT(close()));
+
+    QHBoxLayout *horizontalLayout = new QHBoxLayout;
+    horizontalLayout->addWidget(contentsWidget);
+    horizontalLayout->addWidget(pagesWidget, 1);
+
+    QHBoxLayout *buttonsLayout = new QHBoxLayout;
+    buttonsLayout->addStretch(1);
+    buttonsLayout->addWidget(closeButton);
+
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    mainLayout->addLayout(horizontalLayout);
+    mainLayout->addStretch(1);
+    mainLayout->addSpacing(12);
+    mainLayout->addLayout(buttonsLayout);
+    setLayout(mainLayout);
+
+    setWindowTitle(tr("Intro Program Options"));
+}
+
+void ConfigDialog::CloseDialog()
+{
+
+}
+
+void ConfigDialog::accepted()
+{
+    //emit newDiagramType(BaseNode::UseCase);
+}
+
+void ConfigDialog::createIcons()
+{
+    QListWidgetItem *newbutton = new QListWidgetItem(contentsWidget);
+    newbutton->setIcon(QIcon(":/Images/stickman.png"));
+    newbutton->setText(tr("New"));
+    newbutton->setTextAlignment(Qt::AlignHCenter);
+    newbutton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+
+    QListWidgetItem *openbutton = new QListWidgetItem(contentsWidget);
+    openbutton->setIcon(QIcon(":/Images/diamond.png"));
+    openbutton->setText(tr("Open"));
+    openbutton->setTextAlignment(Qt::AlignHCenter);
+    openbutton->setFlags(Qt::ItemIsSelectable | Qt::ItemIsEnabled);
+
+    connect(contentsWidget,
+            SIGNAL(currentItemChanged(QListWidgetItem*,QListWidgetItem*)),
+            this, SLOT(changePage(QListWidgetItem*,QListWidgetItem*)));
+}
+
+void ConfigDialog::changePage(QListWidgetItem *current, QListWidgetItem *previous)
+{
+    if (!current)
+        current = previous;
+
+    pagesWidget->setCurrentIndex(contentsWidget->row(current));
+}
+
+
---
+++ src/dialog.h
@@ -0,0 +1,33 @@
+#ifndef DIALOGUE_H
+#define DIALOGUE_H
+
+#include <QDialog>
+
+QT_BEGIN_NAMESPACE
+class QListWidget;
+class QListWidgetItem;
+class QStackedWidget;
+QT_END_NAMESPACE
+
+class ConfigDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    ConfigDialog();
+
+public slots:
+    void changePage(QListWidgetItem *current, QListWidgetItem *previous);
+    void CloseDialog();
+    void accepted();
+
+private:
+    void createIcons();
+
+
+    QListWidget *contentsWidget;
+    QStackedWidget *pagesWidget;
+};
+
+#endif // DIALOGUE_H
+
