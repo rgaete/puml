8f5b431af67cd26e4b17171ab62774f4059309f1
Logan Evans <loganpevans@gmail.com>
2012-03-04 20:42:31 -0800

    Readded doxygen. I fixed an issue inside the pUML_doxyfile where moc files were being parsed.

--- doc/misc/activity_graphs.rb
+++
@@ -1,8 +0,0 @@
-%x/hg activity --showtags --datemin=2012-01-15 --datemax=2012-03-04/
-%x/mv activity.png activity_overall.png/
-%x/hg activity --showtags --split=authors --maxcaptions=9 --datemin=2012-01-15 --datemax=2012-03-04/
-%x/mv activity.png activity_authors.png/
-%x/hg activity --showtags --split=files --maxcaptions=9 --datemin=2012-01-15 --datemax=2012-03-04/
-%x/mv activity.png activity_files.png/
-%x/hg activity --showtags --split=directories --datemin=2012-01-15 --datemax=2012-03-04/
-%x/mv activity.png activity_directories.png/
