d84a530fba2dc79c97b5655e77021d440ea56fd7
Zachary Curtis <curt3897@vandals.uidaho.edu>
2012-03-04 18:16:44 -0800

    uploading the progress on the collab diagrams

---
+++ src/UMLnodes_class.h
@@ -0,0 +1,65 @@
+// Copyright (C) 2011-2012 pUML Group
+
+/*! @file UMlnodes_class.h
+    @brief Defines concrete Objectnodes and Connectionnodes that specific to
+           class diagrams.
+  */
+#ifndef SRC_UMLNODES_CLASS_H_
+#define SRC_UMLNODES_CLASS_H_
+
+#include <QInputDialog>
+#include "./nodes.h"
+
+/*! @brief This defines a custom dialog for a ClassBoxObject
+*/
+class ClassBoxObjectDialog : public QInputDialog {
+  Q_OBJECT
+
+  public:
+    explicit ClassBoxObjectDialog(QWidget *parent = 0);
+};
+
+/*!
+ * @brief This concrete class is the ClassBox node.
+ * @sa ObjectNode
+ */
+class ClassBoxObject: public ObjectNode {
+  Q_OBJECT
+
+  public:
+    ClassBoxObject();
+    BaseNode* clone() { return new ClassBoxObject; }
+    QDialog* getDialog();
+    QString getIconPath() { return QString(":/Images/rectangle.png"); }
+    QString getText() { return "ClassBox"; }
+    DiagramType getDiagramType() { return Class; }
+
+    void draw(QPainter &painter);  // NOLINT
+
+
+  public slots:
+    void setName(QString newName) { this->name = newName; }
+
+  private:
+    int radius;
+    QString name;
+};
+
+/*! @brief The properties dialog for class connections
+ */
+class ClassConnectionDialog : public QInputDialog {
+};
+
+/*! @brief This concrete class defines ...
+ */
+class ClassConnection : public SquareConnectionNode {
+  public:
+    BaseNode* clone() { return new ClassConnection; }
+    QDialog* getDialog() { return new ClassConnectionDialog; }
+    QString getIconPath() { return QString(":/Images/interaction.png"); }
+    QString getText() { return "Class Line"; }
+    void draw(QPainter& painter);  // NOLINT
+    DiagramType getDiagramType() { return Class; }
+};
+
+#endif  // SRC_UMLNODES_CLASS_H_
---
+++ src/dialog.h
@@ -0,0 +1,40 @@
+// Copyright (C) 2011-2012 pUML Group
+
+#ifndef SRC_DIALOG_H_
+#define SRC_DIALOG_H_
+
+#include <QDialog>
+
+#include "./dialogpages.h"
+#include "./nodes.h"
+
+QT_BEGIN_NAMESPACE
+class QListWidget;
+class QListWidgetItem;
+class QStackedWidget;
+QT_END_NAMESPACE
+
+class ConfigDialog : public QDialog {
+  Q_OBJECT
+
+  public:
+    ConfigDialog();
+    CreateNewPage *Newpage;
+    CreateOpenPage *Openpage;
+
+  public slots:
+    void changePage(QListWidgetItem *current, QListWidgetItem *previous);
+    void CloseDialog();
+    void AcceptValue(int diagramenum);
+    void accepted();
+
+ signals:
+    void newDiagramType(ObjectNode::DiagramType diagramType);
+
+  private:
+    void createIcons();
+    QListWidget *contentsWidget;
+    QStackedWidget *pagesWidget;
+};
+
+#endif  // SRC_DIALOG_H_
