3afd8abfc5ccccfd34c6f8150217b5460dd5eeb5
Josh Armstrong <theartistformerlyknownasjosh@gmail.com>
2012-03-07 12:13:51 -0800

    Nodes can now return null for their properties dialog, if they have no dialog defined. Fixed all warnings. Fixed cpplint errors.

---
+++ src/UMLnodes.h
@@ -0,0 +1,22 @@
+// Copyright (C) 2011-2012 pUML Group
+
+/*! @file UMLnodes.h
+    @brief Includes all the different types of UML nodes.
+
+    Some naming conventions to keep the code orderly:
+
+    Parentclass:    Naming convention:      Example:
+    ObjectNode      <nodename>Node          OvalObject, StickPersonObject
+    ConnectionNode  <nodename>Connection    InteractionConnection
+    QInputDialog    <nodename>Dialog        OvalObjectDialog, InteractionConnectionDialog
+
+*/
+
+#ifndef SRC_UMLNODES_H_
+#define SRC_UMLNODES_H_
+
+#include "./UMLnodes_usecase.h"
+#include "./UMLnodes_class.h"
+#include "./UMLnodes_state.h"
+
+#endif  // SRC_UMLNODES_H_
