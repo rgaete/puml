68057a1f10b0cfe9cdd7d6cb80dae7cca696c279
Zach <Zach@Mini-Destroyer.ftrdhcpuser.net>
2012-03-14 13:41:16 -0700

    Got parallel arrow connectors for the collab diagrams working.

--- src/UMLnodes_collab.cpp
+++ src/UMLnodes_collab.cpp
@@ -117,6 +117,7 @@ void StickPersonCollabObject::draw(QPainter &painter) {  // NOLINT
   }
 }
 
+
 /*! The StickDialog constructor initializes the dialog and
   passes parent to QInputDialog.
 */
@@ -229,32 +230,45 @@ void ArrowCollabObject::draw(QPainter &painter) {  // NOLINT
   pen.setWidth(2);
 
   // drawing a StickPersonCollab
-  int tempx = position.x();
-  int tempy = position.y();
+  //int tempx = position.x();
+  //int tempy = position.y();
 
-  // background
-  painter.setPen(Qt::NoPen);
-  painter.setBrush(Qt::white);
-  //! @TODO This needs parenthesis or else needs to be fixed.  // NOLINT
 
-  painter.drawLine(tempx - 1/2 *length,
-                   tempy - 1/2 * height,
-                   length,
-                   height);
-  // edge
+ // edge
   painter.setPen(pen);
   painter.setBrush(Qt::NoBrush);
   // head
-  painter.drawLine(tempx,
-                   tempy,
-                   tempx + length,
-                   tempy + height);
-
-
+  DrawArrow(painter);
   // painter.drawText(tempx-10,tempy+50,this->name);
-  painter.drawText(QRect(tempx - length / 2, tempy + height / 2, length, 50),
+  /*painter.drawText(QRect(tempx - length / 2, tempy + height / 2, length, 50),
                    Qt::AlignCenter | Qt::AlignTop | Qt::TextDontClip,
                    this->name);
+*/
+}
+
+void ArrowCollabObject::DrawArrow(QPainter &painter)
+{
+    int tempx = position.x();
+    int tempy = position.y();
+    int headx = tempx + length/2;
+    int heady = tempy;
+
+    painter.drawLine(tempx - length/2,
+                     tempy,
+                     headx,
+                     heady);
+
+
+    // Bottom arrow
+    painter.drawLine(headx,
+                     tempy,
+                     tempx + length/4,
+                     heady + height/4);
+    //Top Arrow
+    painter.drawLine(headx,
+                     tempy,
+                     tempx + length/4,
+                     heady - height/4);
 }
 
 ArrowCollabObjectDialog::ArrowCollabObjectDialog(QWidget *parent)
@@ -271,4 +285,168 @@ QDialog *ArrowCollabObject::getDialog() {
   connect(dialog, SIGNAL(textValueSelected(QString)),
           this, SLOT(setName(QString)));
   return dialog;
+}
+
+//square connector
+
+void CollabConnection::draw(QPainter& painter) {  // NOLINT
+    QPoint temppoint1;
+    QPoint temppoint2;
+    QPoint temppoint3;
+    QPoint temppoint4;
+  BaseNode *obj1, *obj2;
+  std::list<BaseNode*>::iterator it = connectedObjects.begin();
+  obj1 = *it;
+  it++;
+  obj2 = *it;
+
+  pt1 = obj1->getClosestConnectionPoint(obj2->getPosition());
+  pt4 = obj2->getClosestConnectionPoint(obj1->getPosition());
+  painter.setPen(Qt::black);
+  if (pt1.x() == obj1 -> getPosition().x()) {  // North or South connection
+    pt2.setY((pt4.y() + pt1.y()) / 2);
+    pt2.setX(pt1.x());
+    pt3.setX(pt4.x());
+    pt3.setY(pt2.y());
+  } else if (pt1.x() != obj1->getPosition().x()) {  // East or West connection
+    pt2.setY(pt1.y());
+    pt2.setX((pt1.x() + pt4.x()) / 2);
+    pt3.setX(pt2.x());
+    pt3.setY(pt4.y());
+  }
+  if (selected == true) {
+    QPen selectPen;
+    selectPen.setWidth(2);
+    selectPen.setColor(Qt::blue);
+    painter.setPen(selectPen);
+    painter.drawLine(pt1, pt2);
+  }
+  painter.drawLine(pt1, pt2);
+  painter.drawLine(pt2, pt3);
+  painter.drawLine(pt3, pt4);
+
+  //These are the initial values i chose to use
+  temppoint1.setX(pt2.x());
+  temppoint1.setY(pt2.y());
+  temppoint2.setX(pt3.x());
+  temppoint2.setY(pt3.y());
+// these ones were required for a bug fix
+  temppoint3.setX(pt1.x());
+  temppoint3.setY(pt1.y());
+  temppoint4.setX(pt4.x());
+  temppoint4.setY(pt4.y());
+    DrawArrow(painter, temppoint1, temppoint2, temppoint3, temppoint4);
+}
+
+QPoint CollabConnection::FindMidPoint(QPoint point1, QPoint point2)
+{
+    QPoint result;
+    int temp1x = point1.x();
+    int temp1y = point1.y();
+    int temp2x = point2.x();
+    int temp2y = point2.y();
+
+    int fx;
+    int fy;
+
+    fx = (temp1x + temp2x)/2;
+    fy = (temp1y + temp2y)/2;
+
+    result.setX(fx);
+    result.setY(fy);
+
+    return result;
+}
+
+int CollabConnection::FindDirection(QPoint point1, QPoint point2, QPoint temppoint1, QPoint temppoint2)
+{
+    QPoint result;
+// inner points
+    int temp1x = point1.x();
+    int temp1y = point1.y();
+    int temp2x = point2.x();
+    int temp2y = point2.y();
+
+// Outer points
+    int temp3x = temppoint1.x();
+    int temp3y = temppoint1.y();
+    int temp4x = temppoint2.x();
+    int temp4y = temppoint2.y();
+
+
+//extra variables
+    int fx;
+    int fy;
+    int gx;
+    int gy;
+    int absfx;
+    int absfy;
+
+    // find differences
+    fx = temp1x - temp2x;
+    fy = temp1y - temp2y;
+
+    gx = temp3x - temp4x;
+    gy = temp3y - temp4y;
+
+
+    // find absolute values
+    absfx = abs(fx);
+    absfy = abs(fy);
+
+    //if point2 is to the right or
+    if((fx > 0))// || (fy < 0))
+    {
+        return -1;
+    }
+    else if(fx == 0)
+    {
+        if(gx > 0)
+        {
+            return -1;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+    else
+        return 1;
+
+}
+
+void CollabConnection::DrawArrow(QPainter &painter, QPoint point1, QPoint point2, QPoint temppoint1,
+                                 QPoint temppoint2)
+{
+    int dir = 1;
+    int length = 50;
+    int height = 50;
+
+    dir = FindDirection(point1, point2, temppoint1, temppoint2);
+
+    length *= dir;
+
+    int tempx = point1.x()+15;
+    int tempy = point1.y()+15;
+    int headx = tempx + length/2;
+    int heady = tempy;
+
+    painter.drawLine(tempx,
+                     tempy,
+                     headx,
+                     heady);
+
+
+    // Bottom arrow
+    painter.drawLine(headx,
+                     tempy,
+                     tempx + length/3,
+                     heady + height/6);
+    //Top Arrow
+    painter.drawLine(headx,
+                     tempy,
+                     tempx + length/3,
+                     heady - height/6);
+
+
 }
