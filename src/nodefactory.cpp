9c97ed4997db8a437f4a42ff235ce2b2a5a58b6c
Zach <Zach@Mini-Destroyer.ftrdhcpuser.net>
2012-03-15 13:16:06 -0700

    trying to merge so tortoise will let me do stuff

---
+++ src/nodefactory.cpp
@@ -0,0 +1,46 @@
+// Copyright (C) 2011-2012 pUML Group
+
+#include "./nodefactory.h"
+#include <vector>
+
+// Initialize the static variables
+NodeFactory* NodeFactory::instance = 0;      // (Note initially null)
+std::vector<BaseNode*> NodeFactory::prototypes;
+
+/*! The destructor frees up the prototypes
+*/
+NodeFactory::~NodeFactory() {
+  // delete the pointers
+  for (int i = 0; i < static_cast<int>(prototypes.size()); i++) {
+    delete prototypes.at(i);
+  }
+}
+
+/*! Registers a new prototype with the factor and returns the
+  unique ID of the prototype.
+*/
+int NodeFactory::registerPrototype(BaseNode *newPrototype) {
+  prototypes.push_back(newPrototype);
+  return prototypes.size() - 1;
+}
+
+/*! Returns a new copy of the prototype specified by prototype_id.
+  Assumes that the id is valid. Functions should use the id
+  returned by registerPrototype.
+*/
+BaseNode *NodeFactory::produce(int prototype_id) {
+  assert(prototype_id >= 0);
+  assert(prototype_id < static_cast<int>(prototypes.size()));
+  return prototypes.at(prototype_id)->clone();
+}
+
+/*! Returns the one instance of the class. Functions should
+  call produce and registerPrototype with this instance.
+*/
+NodeFactory* NodeFactory::getInstance() {
+  if (instance == 0) {
+    instance = new NodeFactory;
+  }
+
+  return instance;
+}
