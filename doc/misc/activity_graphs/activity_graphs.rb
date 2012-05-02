9df085c9304cd31851c7e42674261d4e832ffc12
Logan Evans <loganpevans@gmail.com>
2012-05-02 13:09:40 -0700

    Updated activity graphs. I think.

--- doc/misc/activity_graphs/activity_graphs.rb
+++ doc/misc/activity_graphs/activity_graphs.rb
@@ -1,4 +1,4 @@
-def create_all datemin="2012-01-15", datemax="2012-03-04"
+def create_all datemin="2012-01-15", datemax="2012-05-01"
     %x/hg activity --showtags --datemin=#{datemin} --datemax=#{datemax} --width=1920 --height=1080 --filename=overall.png/
     %x/hg activity --showtags --split=authors --maxcaptions=9 --datemin=#{datemin} --datemax=#{datemax} --width=1920 --height=1080 --filename=authors.png/
     %x/hg activity --showtags --split=files --maxcaptions=9 --datemin=#{datemin} --datemax=#{datemax} --width=1920 --height=1080 --filename=files.png/
